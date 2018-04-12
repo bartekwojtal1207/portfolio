c9707d31902ad1876d6fad87627965399384a6e3
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2018-04-12 23:52:52 +0200

    odstępy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-
 @section('content')
+
     <section class="welcome">
         <div style="position: fixed; z-index: -99; width: 100%; height: 100%; top:0; left: 0;">
             <iframe frameborder="0" height="100%" width="100%"
@@ -15,12 +15,9 @@
                         <h3 class="js-show-text" id="msg"></h3>
                         <a class="welcome-button">zapraszam</a>
                     </div>
-
                 </div>
             </div>
-
         </div>
     </section>
-
 
 @endsection
